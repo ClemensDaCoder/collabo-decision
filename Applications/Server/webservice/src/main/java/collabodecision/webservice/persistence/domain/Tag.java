2dc82760f28a53c4ead36623b503ef37ce267365
rainerangermeier <angermeier.rainer@gmail.com>
2014-11-07 17:52:37 +0100

    Mock Data for MySQL Database. - Removing isPerson property from Tag Domain (Table).

--- Applications/Server/webservice/src/main/java/collabodecision/webservice/persistence/domain/Tag.java
+++ Applications/Server/webservice/src/main/java/collabodecision/webservice/persistence/domain/Tag.java
@@ -17,23 +17,12 @@ public class Tag {
 	@Column(nullable=false, unique=true)
 	private String name;
 	
-	@Column
-	private boolean person;
-
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public boolean isPerson() {
-		return person;
-	}
-
-	public void setPerson(boolean person) {
-		this.person = person;
 	}
 
 	public long getIdTag() {
