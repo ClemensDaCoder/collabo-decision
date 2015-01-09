7838f53325b53e424151e4b53cb19da77f8f4f8d
ManuelHo <manuel.hochreiter@atn.ac.at>
2015-01-09 22:21:12 +0100

    added request for owning issues in dashboard

--- Applications/Server/webservice/src/main/webapp/app/components/dashboard/controllers/dashboardViewController.js
+++ Applications/Server/webservice/src/main/webapp/app/components/dashboard/controllers/dashboardViewController.js
@@ -1,3 +1,14 @@
-angular.moudle('collaboApp').controller("DashboardController", [function() {
+angular.module('collaboApp').controller('DashboardController', ['$http', '$scope', function($http, $scope) {
+	
+	$scope.getOwningIssues = function() {
+		
+		var uri = "/rest/issues?owned=true";
+		
+		$http.get(uri).success(function(data) {
+			$scope.issues = angular.fromJson(data);
+		});
+	};
+	
+	$scope.getOwningIssues();
 	
 }]);
